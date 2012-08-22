7665756f9cbf5f570afe2243d69eb372b1785e62
Alexander Rumyantsev <alexander.rumyantsev@wikimart.ru>
2012-08-22 12:39:30 +0400

    deduplication caching

--- dbwriter.php
+++ dbwriter.php
@@ -13,6 +13,9 @@
         $amqp_sub = new AMQPQueue(new AMQPChannel($amqp_conn));
         $amqp_sub->SetName('filestorage.dbwriter');
 
+        $mc = new Memcache;
+        $cache = $mc->connect($config['memcache']['host'], $config['memcache']['port']);
+
         while($message = $amqp_sub->get()) {
 
             $data = unserialize($message->getBody());
@@ -33,21 +36,20 @@
 
                 if ($data['Action'] == 'copy') {
 
-                    unset($dup);
-                    if($res = mysql_query("SELECT `uuid-text`, `ext` FROM files WHERE `hash` = UNHEX('" .$hash. "') AND `size` = " .$size. " AND `group` = " .$groupindex. " AND `deleted` = 0")) {
-                        if (mysql_num_rows($res) > 0) {
-                            $row = mysql_fetch_assoc($res);
-                            $dst = substr($row['uuid-text'], 32, 2) .'/'. substr($row['uuid-text'], 30, 2) .'/'. $row['uuid-text'] .'.'. $row['ext'];
-                            if ($source != $dst) {
-                                $dup = array();
-                                $dup['Action'] = 'dedup';
-                                $dup['Time'] = time();
-                                $dup['Files'][] = $source;
-                                $dup['Files'][] = $dst;
+                    $dup = FALSE;
+
+                    if($cache) {
+                        $dup = $mc->get("$hash:$size:$groupindex");
+                    }
+
+                    if (!$dup) {
+                        if($res = mysql_query("SELECT `uuid-text`, `ext` FROM files WHERE `hash` = UNHEX('" .$hash. "') AND `size` = " .$size. " AND `group` = " .$groupindex. " AND `deleted` = 0")) {
+                            if (mysql_num_rows($res) > 0) {
+                                $dup = mysql_fetch_assoc($res);
                             }
+                        } else {
+                            throw new Exception("Query error:" . mysql_error());
                         }
-                    } else {
-                        throw new Exception("Query error:" . mysql_error());
                     }
 
                     if(!mysql_query("INSERT IGNORE INTO files(`uuid`,`uuid-text`,`date`, `hash`, `size`, `ext`, `group`) VALUES (UNHEX(REPLACE('". $uuid ."', '-', '')), '". $uuid ."', FROM_UNIXTIME(" .$time. "), UNHEX('" .$hash."'), " .$size. ", '" .$ext. "', " .$groupindex. ")"))
@@ -58,14 +60,25 @@
                             throw new Exception("Cannot insert attribute for UUID $uuid into table attributes: " . mysql_error ($db));
                     }
 
-                    if (isset($dup)) {
-                        mq_send_to_group(serialize($dup), $groupindex);
+                    if($dup) {
+                        $dst = substr($row['uuid-text'], 32, 2) .'/'. substr($row['uuid-text'], 30, 2) .'/'. $row['uuid-text'] .'.'. $row['ext'];
+                        if ($source != $dst) {
+                            mq_send_to_group(serialize(array('Action' => 'dedup', 'Time' => time(), 'Files' => array($source, $dst))), $groupindex);
+                        }
+                    } else {
+                        if ($cache) {
+                            $mc->set("$hash:$size:$groupindex", array('uuid-text' => $uuid, 'ext' => $ext));
+                        }
                     }
                 }
 
                 if ($data['Action'] == 'delete') {
                     if(!mysql_query("UPDATE IGNORE files SET deleted = TRUE, date = FROM_UNIXTIME(" .$time. ") WHERE `uuid` = UNHEX(REPLACE('". $uuid ."', '-', ''))"))
                         throw new Exception("Cannot update file for UUID $uuid into table files: " . mysql_error ($db));
+
+                    if($cache) {
+                        $mc->delete("$hash:$size:$groupindex");
+                    }
                 }
 
                 if(!mysql_query("COMMIT")) throw new Exception("Cannot commit:" . mysql_error ($db));

eff8926f9d8bdb6060396a398688718638d81f80
Alexander Rumyantsev <alexander.rumyantsev@wikimart.ru>
2013-04-08 17:57:31 +0400

    skip checks for standalone modules

--- common.php
+++ common.php
@@ -2,18 +2,9 @@
 
 function init() {
 
-    if (!phpversion('xattr'))
-        throw new Exception('No xattr support in PHP');
-
-    if (!phpversion('amqp'))
-        throw new Exception('No AMQP support in PHP');
-
     global $config;
 
     $config = parse_ini_file('filestorage.ini', true, INI_SCANNER_RAW);
-
-    if (!xattr_supported($config['node']['storage']))
-        throw new Exception('No extended attributes support for "' . $config['node']['lockdir'] . '" or directory not readable');
 
     if ($config['node']['hashalgo'] != 'md5' && $config['node']['hashalgo'] != 'sha256')
         throw new Exception('Hash algorithm "' . $config['node']['hashalgo'] . '" not supported');
